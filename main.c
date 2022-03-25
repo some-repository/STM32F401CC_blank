687779c5caca08646167c9b1139402ce66d236dc
Vladislav <patch.code2002@gmail.com>
2022-03-25 21:52:49 +0300

    Trouble with linking

--- main.c
+++ main.c
@@ -1,34 +1,31 @@
-#include <stm32f401xc.h>
-#include <stm32f4xx_ll_rcc.h>
-#include <stm32f4xx_ll_system.h>
-#include <stm32f4xx_ll_bus.h>
-#include <stm32f4xx_ll_gpio.h>
-//#include <stm32f4xx.h>
+#define USB_OTG_FS
+#define USB_OTG_HS
+#define USE_FULL_LL_DRIVER
+#define RCC
 
-
+#include "stm32f401xc.h"
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_system.h"
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_gpio.h"
 
 /*
  * Clock on GPIOC and set led pin
  */
-static void gpio_config(void)
+void GPIO_config(void)
 {
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
     LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_13, LL_GPIO_MODE_OUTPUT);
 }
 
-/*
- * Just set of commands to waste CPU power for 10ms
- * (basically it is a simple cycle with a predefined number
- * of loops)
- */
-void delay (void)
+static void delay (void)
 {
     int i = 0;
     for (i = 0; i < 1000000; i++);
     i = 0;
 }
 
-void RCC_configuration (void)
+void RCC_config (void)
 {
     LL_FLASH_SetLatency (LL_FLASH_LATENCY_2);
     /* Enable HSE and wait for activation*/
@@ -37,29 +34,38 @@ void RCC_configuration (void)
 
     LL_RCC_PLL_Disable ();
     LL_RCC_PLL_ConfigDomain_SYS (LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_25, 336, LL_RCC_PLLP_DIV_4); //336 is PLLN
-    L_RCC_PLL_ConfigDomain_48M (LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_25, 336, LL_RCC_PLLQ_DIV_7);
+    LL_RCC_PLL_ConfigDomain_48M (LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_25, 336, LL_RCC_PLLQ_DIV_7);
     LL_RCC_PLL_Enable ();
     while (LL_RCC_PLL_IsReady () != 1);
+
     LL_RCC_SetAHBPrescaler (LL_RCC_SYSCLK_DIV_1);
     LL_RCC_SetAPB1Prescaler (LL_RCC_APB1_DIV_2);
     LL_RCC_SetAPB2Prescaler (LL_RCC_APB2_DIV_1);
-    L_RCC_SetSysClkSource (LL_RCC_SYS_CLKSOURCE_PLL);
-    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
-
+    LL_RCC_SetSysClkSource (LL_RCC_SYS_CLKSOURCE_PLL);
+    while (LL_RCC_GetSysClkSource () != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
     while (LL_RCC_GetUSBClockFreq (LL_RCC_USB_CLKSOURCE) == LL_RCC_PERIPH_FREQUENCY_NO);
+}
 
-    LL_RCC_ConfigMCO (LL_RCC_MCO2SOURCE_SYSCLK, LL_RCC_MCO2_DIV_2);
+void MCO_config (void) //SYSCLK/2 on PA8
+{
+    LL_AHB2_GRP1_EnableClock (LL_AHB1_GRP1_PERIPH_GPIOA);
+    LL_GPIO_SetPinMode (GPIOA, LL_GPIO_PIN_8, LL_GPIO_MODE_ALTERNATE);
+    LL_GPIO_SetPinSpeed (GPIOA, LL_GPIO_PIN_8, LL_GPIO_SPEED_FREQ_VERY_HIGH);
+    LL_GPIO_SetAFPin_8_15 (GPIOA, LL_GPIO_PIN_8, LL_GPIO_AF_0);
+    LL_RCC_ConfigMCO (LL_RCC_MCO2SOURCE_SYSCLK, LL_RCC_MCO2_DIV_2);   
 }
 
 int main(void)
 {
-    gpio_config();
+    RCC_config ();
+    GPIO_config ();
+    MCO_config ();
 
     while (1) 
     {
-        LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_13);
-        delay();
-        LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_13);
-        delay();
+        LL_GPIO_SetOutputPin (GPIOC, LL_GPIO_PIN_13);
+        delay ();
+        LL_GPIO_ResetOutputPin (GPIOC, LL_GPIO_PIN_13);
+        delay ();
     }
 }
