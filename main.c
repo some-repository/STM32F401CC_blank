1c652a3d4523317428dd43a381f72dc347595e47
Vladislav <patch.code2002@gmail.com>
2022-03-25 04:06:23 +0300

    Started RCC configuring

--- main.c
+++ main.c
@@ -28,6 +28,29 @@ void delay (void)
     i = 0;
 }
 
+void RCC_configuration (void)
+{
+    LL_FLASH_SetLatency (LL_FLASH_LATENCY_2);
+    /* Enable HSE and wait for activation*/
+    LL_RCC_HSE_Enable ();
+    while (LL_RCC_HSE_IsReady () != 1);
+
+    LL_RCC_PLL_Disable ();
+    LL_RCC_PLL_ConfigDomain_SYS (LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_25, 336, LL_RCC_PLLP_DIV_4); //336 is PLLN
+    L_RCC_PLL_ConfigDomain_48M (LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_25, 336, LL_RCC_PLLQ_DIV_7);
+    LL_RCC_PLL_Enable ();
+    while (LL_RCC_PLL_IsReady () != 1);
+    LL_RCC_SetAHBPrescaler (LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetAPB1Prescaler (LL_RCC_APB1_DIV_2);
+    LL_RCC_SetAPB2Prescaler (LL_RCC_APB2_DIV_1);
+    L_RCC_SetSysClkSource (LL_RCC_SYS_CLKSOURCE_PLL);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+
+    while (LL_RCC_GetUSBClockFreq (LL_RCC_USB_CLKSOURCE) == LL_RCC_PERIPH_FREQUENCY_NO);
+
+    LL_RCC_ConfigMCO (LL_RCC_MCO2SOURCE_SYSCLK, LL_RCC_MCO2_DIV_2);
+}
+
 int main(void)
 {
     gpio_config();
